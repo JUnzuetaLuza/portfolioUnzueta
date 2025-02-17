56cecc6f8f03c3093c312b4b8b6ae3ebcd0ad884
JUnzuetaLuza <joseunz2k@gmail.com>
2025-02-17 12:49:44 -0500

    feat: links per project

--- src/components/WorkModal/WorkModal.jsx
+++ src/components/WorkModal/WorkModal.jsx
@@ -1,4 +1,7 @@
 import css from './WorkModal.module.css'
+import github from '../../assets/github.png';
+import youtube from '../../assets/youtube.png';
+import www from '../../assets/www.png';
 
 export const WorkModal = ({ isOpen, onClose, work}) => {
     if (!isOpen) return null;
@@ -8,11 +11,21 @@ export const WorkModal = ({ isOpen, onClose, work}) => {
             <div className={css.modal}>
                 <h2>{work.name}</h2>
                 <p>{work.description}</p>
-                <ul>
-                    {work.skills.map((skill, index) => (
-                        <li key={index}>{skill}</li>
-                    ))}
-                </ul>
+                <div>
+                    <ul className={css.skillsModal}>
+                        {work.skills.map((skill, index) => (
+                            <li key={index}>{skill}</li>
+                        ))}
+                    </ul>
+                    <ul className={css.linksModal}>
+                        {work.github ? 
+                        <a href={work.github} target="_blank" rel="noopener noreferrer"><img src={github}/></a> : <></> }
+                        {work.youtube ? 
+                        <a href={work.youtube} target="_blank" rel="noopener noreferrer"><img src={youtube}/></a> : <></> }
+                        {work.web ? 
+                        <a href={work.web} target="_blank" rel="noopener noreferrer"><img src={www}/></a> : <></> }
+                    </ul>
+                </div>
                 <button onClick={onClose}>Cerrar</button>
             </div>
         </div>
